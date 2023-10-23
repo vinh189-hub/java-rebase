d8709e8c68c11c790d5ae03cdf0c50a58f2fa3dd
Vinh Ninh Xuan <vinhnx@hblab.vn>
2023-10-23 17:08:22 +0700

    get link

--- demo-rebase/src/CrawlVideo.java
+++ demo-rebase/src/CrawlVideo.java
@@ -3,4 +3,8 @@ public class CrawlVideo {
     public static void main(String[] args) {
         System.out.println("Show link video");
     }
+
+    public void getLink(){
+        System.out.println("Get Links");
+    }
 }
