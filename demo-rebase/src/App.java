6e82c8fe7b81b184bd900325df2c65d78670ef2b
Vinh Ninh Xuan <vinhnx@hblab.vn>
2023-10-23 16:56:53 +0700

    new feature

--- demo-rebase/src/App.java
+++ demo-rebase/src/App.java
@@ -2,4 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Developer !");
     }
+
+    public void getApi(){
+        System.out.println("Get API");
+    }
 }
